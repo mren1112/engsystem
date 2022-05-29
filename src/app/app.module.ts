c29d5b9c108f2f39afa7f1092161720143ed4285
mren1112 <awongod@gmail.com>
2022-05-29 11:42:49 +0700

    add new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { BackButtonDisableModule } from 'angular-disable-browser-back-button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+ /*  { path: 'course', component: CourseComponent },
+    { path: "course", loadChildren: () => import('./component/couseselect/course.component').then(m => m.CourseComponent),
+  },*/
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   // BackButtonDisableModule.forRoot({ preserveScrollPosition: true }),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
